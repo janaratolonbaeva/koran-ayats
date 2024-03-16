faa648d47730bf1b447cfa25908abeb5bbea2b43
Janara Tolonbaeva <janaratolonbaeva@gmail.com>
2024-03-16 16:52:45 +0600

    Revert "Added adobe lib"
    This reverts commit 22115efc785cacdbcdd752854176197394ce120d.

--- index.js
+++ index.js
@@ -3,110 +3,73 @@ window.addEventListener('DOMContentLoaded', () => {
   const content = document.querySelector('.content');
   const buttons = document.querySelector('.buttons');
 
-  // function createIframe() {
-  //   for (i = 0; i < filesNumber; i++) {
-  //     const url = `https://janaratolonbaeva.github.io/koran-ayats/files/lesson-${i + 1}.pdf#view=fitH`;
-  //     const iframe = document.createElement('iframe');
+  function createIframe() {
+    for (i = 0; i < filesNumber; i++) {
+      const url = `https://janaratolonbaeva.github.io/koran-ayats/files/lesson-${i + 1}.pdf#view=fitH`;
+      const iframe = document.createElement('iframe');
 
-  //     iframe.setAttribute('src', url);
-  //     iframe.setAttribute('scrolling', 'auto');
-  //     iframe.setAttribute('type', 'application/pdf');
-  //     iframe.setAttribute('allowfullscreen', true);
-  //     iframe.setAttribute('class', `tab-content w-full min-h-screen`);
-  //     if (i !== 0) {
-  //       iframe.style.display = 'none';
-  //     }
-  //     iframe.setAttribute('id', `tab-content-${i + 1}`);
-  //     content.appendChild(iframe);
-  //   }
-  // }
+      iframe.setAttribute('src', url);
+      iframe.setAttribute('scrolling', 'auto');
+      iframe.setAttribute('type', 'application/pdf');
+      iframe.setAttribute('class', `tab-content w-full min-h-screen`);
+      if (i !== 0) {
+        iframe.style.display = 'none';
+      }
+      iframe.setAttribute('id', `tab-content-${i + 1}`);
+      content.appendChild(iframe);
+    }
+  }
 
-  // createIframe();
+  createIframe();
 
-  // const adobeDCView = new AdobeDC.View({ clientId: '4526a0192e9a40de9b45c4cad96b5888', divId: 'content' });
+  function createButton() {
+    for (i = 0; i < filesNumber; i++) {
+      const button = document.createElement('button');
 
-  document.addEventListener('adobe_dc_view_sdk.ready', function () {
-    function createContentItem(i) {
-      const contentItem = document.createElement('div');
-
-      contentItem.setAttribute('class', 'tab-content w-full min-h-[80vh]');
-      contentItem.setAttribute('id', `tab-content-${i + 1}`);
+      button.setAttribute(
+        'class',
+        `tab-btn tab-${i + 1} w-fit min-w-[30px] h-[30px] text-center heading-[30px] rounded-sm px-2 py-1`
+      );
 
       if (i !== 0) {
-        contentItem.style.display = 'none';
+        button.classList.add('bg-[#ccc]', 'text-[#6f491e]');
+      } else {
+        button.classList.add('bg-[#6f491e]', 'text-white');
       }
 
-      console.log('tab-content-${i + 1}', `tab-content-${i + 1}`);
+      button.innerHTML = `PDF ${i + 1}`;
+      buttons.appendChild(button);
+    }
+  }
 
-      if (contentItem) {
-        content.appendChild(contentItem);
+  createButton();
 
-        var adobeDCView = new AdobeDC.View({
-          clientId: '4526a0192e9a40de9b45c4cad96b5888',
-          divId: `tab-content-${i + 1}`,
-        });
+  function openTab(evt, tabName) {
+    var i, tabContent, tabButtons;
 
-        adobeDCView.previewFile({
-          content: { location: { url: `https://janaratolonbaeva.github.io/koran-ayats/files/lesson-${i + 1}.pdf` } },
-          metaData: { fileName: `lesson-${i + 1}.pdf` },
-        });
-      }
+    tabContent = document.getElementsByClassName('tab-content');
+    for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = 'none';
     }
 
-    for (i = 0; i < filesNumber; i++) {
-      createContentItem(i);
+    tabButtons = document.getElementsByClassName('tab-btn');
+    for (i = 0; i < tabButtons.length; i++) {
+      tabButtons[i].classList.remove('bg-[#6f491e]', 'text-white');
+      tabButtons[i].classList.add('bg-[#ccc]', 'text-[#6f491e]');
     }
+    const iframe = document.getElementById(tabName);
 
-    function createButton() {
-      for (i = 0; i < filesNumber; i++) {
-        const button = document.createElement('button');
+    iframe.style.display = 'block';
 
-        button.setAttribute(
-          'class',
-          `tab-btn tab-${i + 1} w-fit min-w-[30px] h-[30px] text-center heading-[30px] rounded-sm px-2 py-1`
-        );
+    evt.currentTarget.classList.remove('bg-[#ccc]', 'text-[#6f491e]');
+    evt.currentTarget.classList.add('bg-[#6f491e]', 'text-white');
+  }
 
-        if (i !== 0) {
-          button.classList.add('bg-[#ccc]', 'text-[#6f491e]');
-        } else {
-          button.classList.add('bg-[#6f491e]', 'text-white');
-        }
+  const buttonItems = document.querySelectorAll('.tab-btn');
 
-        button.innerHTML = `PDF ${i + 1}`;
-        buttons.appendChild(button);
-      }
-    }
-
-    createButton();
-
-    function openTab(evt, tabName) {
-      var i, tabContent, tabButtons;
-
-      tabContent = document.getElementsByClassName('tab-content');
-
-      for (i = 0; i < tabContent.length; i++) {
-        tabContent[i].style.display = 'none';
-      }
-
-      tabButtons = document.getElementsByClassName('tab-btn');
-      for (i = 0; i < tabButtons.length; i++) {
-        tabButtons[i].classList.remove('bg-[#6f491e]', 'text-white');
-        tabButtons[i].classList.add('bg-[#ccc]', 'text-[#6f491e]');
-      }
-      const iframe = document.getElementById(tabName);
-
-      iframe.style.display = 'block';
-
-      evt.currentTarget.classList.remove('bg-[#ccc]', 'text-[#6f491e]');
-      evt.currentTarget.classList.add('bg-[#6f491e]', 'text-white');
-    }
-
-    const buttonItems = document.querySelectorAll('.tab-btn');
-
-    buttonItems.forEach((btn, index) => {
-      btn.addEventListener('click', (e) => {
-        openTab(e, `tab-content-${index + 1}`);
-      });
+  buttonItems.forEach((btn, index) => {
+    btn.addEventListener('click', (e) => {
+      openTab(e, `tab-content-${index + 1}`);
     });
   });
 });
